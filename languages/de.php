6b3e967964d379ea8ef5044840ef09300c1eb230
Jonas Döbertin <hello@jd-powered.net>
2015-04-16 12:38:52 +0200

    Added German language file

---
+++ languages/de.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+    /* Main toolbar icons */
+    'action.tooltip.header1'       => 'Hauptüberschrift',
+    'action.tooltip.header2'       => 'Unterüberschrift',
+    'action.name.header2'          => 'h',
+    'action.tooltip.bold'          => 'Fett',
+    'action.tooltip.italicize'     => 'Kursiv',
+    'action.tooltip.blockquote'    => 'Zitat',
+    'action.tooltip.unorderedList' => 'Unsortierte Liste',
+    'action.tooltip.orderedList'   => 'Sortierte Liste',
+    'action.tooltip.link'          => 'Link einfügen',
+    'action.tooltip.image'         => 'Bild einfügen',
+    'action.tooltip.line'          => 'Horizontale Linie einfügen',
+    'action.tooltip.fullscreen'    => 'Vollbildmodus ein-/ausschalten',
+
+    /* Help icon subitems */
+    'action.tooltip.markdownLink'  => 'Alles wissenswerte über Markdown',
+    'action.name.markdownLink'     => 'Markdown Hilfe',
+    'action.tooltip.kirbytextLink' => 'Textauszeichnung mit Kirbytext',
+    'action.name.kirbytextLink'    => 'Kirbytext Hilfe',
+    'action.tooltip.issuesLink'    => 'Einen Fehler auf Github melden',
+    'action.name.issuesLink'       => 'Einen Fehler melden',
+
+);
--- languages/en.php
+++ languages/en.php
@@ -1,6 +1,8 @@
 <?php
 
 return array(
+
+    /* Main toolbar icons */
     'action.tooltip.header1'       => 'Primary Headline',
     'action.tooltip.header2'       => 'Secondary Headline',
     'action.name.header2'          => 'h',
@@ -12,12 +14,14 @@ return array(
     'action.tooltip.link'          => 'Insert Link',
     'action.tooltip.image'         => 'Insert Image',
     'action.tooltip.line'          => 'Insert Horizontal Line',
-    'action.tooltip.fullscreen'    => 'Toggle fullscreen mode',
+    'action.tooltip.fullscreen'    => 'Toggle Fullscreen Mode',
 
+    /* Help icon subitems */
     'action.tooltip.markdownLink'  => 'All you need to know about Markdown',
     'action.name.markdownLink'     => 'Markdown Help',
     'action.tooltip.kirbytextLink' => 'Text formatting with Kirbytext',
     'action.name.kirbytextLink'    => 'Kirbytext Help',
     'action.tooltip.issuesLink'    => 'Open an issue on Github',
     'action.name.issuesLink'       => 'Report a Bug',
+    
 );
