f989aa3b5d373d5c9b65fc691dc1f3f1441f66cb
Martin <ole-martin@bratteng.me>
2015-06-17 12:38:36 +0200

    Added Norwegian Bokmål Translation
    Signed-off-by: Martin <ole-martin@bratteng.me>

---
+++ languages/no_nb.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /* Main toolbar icons */
+    'action.tooltip.header1'       => 'Tittel',
+    'action.tooltip.header2'       => 'Under Tittel',
+    'action.name.header2'          => 'h',
+    'action.tooltip.bold'          => 'Fet',
+    'action.tooltip.italicize'     => 'Kursiv',
+    'action.tooltip.strikethrough' => 'Gjennomstreket',
+    'action.tooltip.blockquote'    => 'Sitat',
+    'action.tooltip.unorderedList' => 'Usortert Liste',
+    'action.tooltip.orderedList'   => 'Sortert Liste',
+    'action.tooltip.link'          => 'Sett Inn Link',
+    'action.tooltip.image'         => 'Sett Inn Bilde',
+    'action.tooltip.line'          => 'Sett Inn Horisontal Linje',
+    'action.tooltip.fullscreen'    => 'Aktiver Fullskjerms Modus',
+
+    /* Help icon subitems */
+    'action.tooltip.shortcutsModal' => 'Alle tilgjengelige tastatur snarveier',
+    'action.name.shortcutsModal'    => 'Tastatur Snarveier',
+    'action.tooltip.markdownLink'   => 'Alt du trenger å vite om Markdown',
+    'action.name.markdownLink'      => 'Markdown Hjelp',
+    'action.tooltip.kirbytextLink'  => 'Tekst formatering med Kirbytext',
+    'action.name.kirbytextLink'     => 'Kirbytext Hjelp',
+    'action.tooltip.issuesLink'     => 'Åpne en rapport på GitHub',
+    'action.name.issuesLink'        => 'Rapporter en feil',
+    'action.tooltip.licenseLink'    => 'Støtt videre utvikling med å kjøpe en moral lisens',
+    'action.name.licenseLink'       => 'Kjøp en lisens',
+
+
+    /* Shortcuts modal texts */
+    'modal.shortcuts.title'                => 'Tastatur Snarveier',
+    'modal.shortcuts.heading.action'       => 'Handling',
+    'modal.shortcuts.heading.shortcut'     => 'Snarvei',
+    'modal.shortcuts.action.heading'       => 'Tittel',
+    'modal.shortcuts.action.subheading'    => 'Under Tittel',
+    'modal.shortcuts.action.bold'          => 'Fet',
+    'modal.shortcuts.action.italic'        => 'Kursiv',
+    'modal.shortcuts.action.strikethrough' => 'Gjennomstreket',
+    'modal.shortcuts.action.blockquote'    => 'Sitat',
+    'modal.shortcuts.action.unorderedList' => 'Usortert Liste',
+    'modal.shortcuts.action.orderedList'   => 'Sortert Liste',
+    'modal.shortcuts.action.image'         => 'Bilde',
+    'modal.shortcuts.action.link'          => 'Link',
+
+);
