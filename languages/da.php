2f14a361e4fda94e1c1f957b85e235680436c8d8
Martin <ole-martin@bratteng.me>
2015-06-17 13:12:46 +0200

    Added Swedish and Danish language support. Updated Norwegian typo
    Signed-off-by: Martin <ole-martin@bratteng.me>

---
+++ languages/da.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @language Dansk
+ * @direction ltr
+ * @author Ole-Martin Bratteng <ole-martin@bratteng.me>
+ * @version 1.0.0
+**/
+
+return array(
+
+    /* Main toolbar icons */
+    'action.tooltip.header1'       => 'Titel',
+    'action.tooltip.header2'       => 'Undertitel',
+    'action.name.header2'          => 'h',
+    'action.tooltip.bold'          => 'Fet',
+    'action.tooltip.italicize'     => 'Kursiv',
+    'action.tooltip.strikethrough' => 'Gennemstreget',
+    'action.tooltip.blockquote'    => 'Citat',
+    'action.tooltip.unorderedList' => 'Usorteret Liste',
+    'action.tooltip.orderedList'   => 'Sorteret Liste',
+    'action.tooltip.link'          => 'Indsæt Link',
+    'action.tooltip.image'         => 'Indsæt Billede',
+    'action.tooltip.line'          => 'Indsæt Vandret Linje',
+    'action.tooltip.fullscreen'    => 'Aktiver Fuld Skærm',
+
+    /* Help icon subitems */
+    'action.tooltip.shortcutsModal' => 'Alle tilgængelige tastaturgenveje',
+    'action.name.shortcutsModal'    => 'Tastaturgenveje',
+    'action.tooltip.markdownLink'   => 'Alt hvad du behøver at vide om Markdown',
+    'action.name.markdownLink'      => 'Markdown Hjælp',
+    'action.tooltip.kirbytextLink'  => 'Formatering af tekst Kirbytext',
+    'action.name.kirbytextLink'     => 'Kirbytext Hjælp',
+    'action.tooltip.issuesLink'     => 'Åbn en rapport om GitHub',
+    'action.name.issuesLink'        => 'Rapporter en fejl',
+    'action.tooltip.licenseLink'    => 'Støtte udviklingen ved at købe en moralsk licens',
+    'action.name.licenseLink'       => 'Køb en licens',
+
+
+    /* Shortcuts modal texts */
+    'modal.shortcuts.title'                => 'Tastaturgenveje',
+    'modal.shortcuts.heading.action'       => 'Handling',
+    'modal.shortcuts.heading.shortcut'     => 'Genvej',
+    'modal.shortcuts.action.heading'       => 'Titel',
+    'modal.shortcuts.action.subheading'    => 'Undertitel',
+    'modal.shortcuts.action.bold'          => 'Fet',
+    'modal.shortcuts.action.italic'        => 'Kursiv',
+    'modal.shortcuts.action.strikethrough' => 'Gennemstreget',
+    'modal.shortcuts.action.blockquote'    => 'Citat',
+    'modal.shortcuts.action.unorderedList' => 'Usorteret Liste',
+    'modal.shortcuts.action.orderedList'   => 'Sorteret Liste',
+    'modal.shortcuts.action.image'         => 'Billede',
+    'modal.shortcuts.action.link'          => 'Link',
+
+);
--- languages/no_nb.php
+++ languages/no_nb.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * @language Norsk Bokmål
+ * @direction ltr
+ * @author Ole-Martin Bratteng <ole-martin@bratteng.me>
+ * @version 1.1.0
+**/
 
 return array(
 
     /* Main toolbar icons */
     'action.tooltip.header1'       => 'Tittel',
-    'action.tooltip.header2'       => 'Under Tittel',
+    'action.tooltip.header2'       => 'Undertittel',
     'action.name.header2'          => 'h',
     'action.tooltip.bold'          => 'Fet',
     'action.tooltip.italicize'     => 'Kursiv',
@@ -18,24 +24,24 @@ return array(
     'action.tooltip.fullscreen'    => 'Aktiver Fullskjerms Modus',
 
     /* Help icon subitems */
-    'action.tooltip.shortcutsModal' => 'Alle tilgjengelige tastatur snarveier',
-    'action.name.shortcutsModal'    => 'Tastatur Snarveier',
+    'action.tooltip.shortcutsModal' => 'Alle tilgjengelige tastatursnarveier',
+    'action.name.shortcutsModal'    => 'Tastatursnarveier',
     'action.tooltip.markdownLink'   => 'Alt du trenger å vite om Markdown',
     'action.name.markdownLink'      => 'Markdown Hjelp',
     'action.tooltip.kirbytextLink'  => 'Tekst formatering med Kirbytext',
     'action.name.kirbytextLink'     => 'Kirbytext Hjelp',
     'action.tooltip.issuesLink'     => 'Åpne en rapport på GitHub',
     'action.name.issuesLink'        => 'Rapporter en feil',
-    'action.tooltip.licenseLink'    => 'Støtt videre utvikling med å kjøpe en moral lisens',
+    'action.tooltip.licenseLink'    => 'Støtt videre utvikling ved å kjøpe en moral lisens',
     'action.name.licenseLink'       => 'Kjøp en lisens',
 
 
     /* Shortcuts modal texts */
-    'modal.shortcuts.title'                => 'Tastatur Snarveier',
+    'modal.shortcuts.title'                => 'Tastatursnarveier',
     'modal.shortcuts.heading.action'       => 'Handling',
     'modal.shortcuts.heading.shortcut'     => 'Snarvei',
     'modal.shortcuts.action.heading'       => 'Tittel',
-    'modal.shortcuts.action.subheading'    => 'Under Tittel',
+    'modal.shortcuts.action.subheading'    => 'Undertittel',
     'modal.shortcuts.action.bold'          => 'Fet',
     'modal.shortcuts.action.italic'        => 'Kursiv',
     'modal.shortcuts.action.strikethrough' => 'Gjennomstreket',
